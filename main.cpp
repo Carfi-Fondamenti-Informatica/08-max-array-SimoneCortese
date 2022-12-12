ca438c6568398b6a887ceb6e75291f508a20be8b
SimoneCortese <117988407+SimoneCortese@users.noreply.github.com>
2022-12-12 15:48:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  int A;
+  cin>>A;
+  float num[A];
+  for (int i=0;i<A;i++){
+      cin>>num[i];
+  }
+  cout<<funzione(num,A);
   return 0;
 }
