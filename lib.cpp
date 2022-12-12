b7f9c86122c20f1efb11ad63903fe747d245f915
SimoneCortese <117988407+SimoneCortese@users.noreply.github.com>
2022-12-12 15:44:52 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,10 @@
-
+#include "lib.h"
+float funzione(float numeri[], int dimensione){
+float massimo = numeri[0];
+  for (int i=0;i<dimensione;i++){
+  if (massimo < numeri[i]{
+      massimo = numeri[i];
+      }
+      }
+      return massimo;
+}
